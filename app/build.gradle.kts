41725ead80dd595f12b13f9cf22a6deface8f680
战神谭 <2764732335@qq.com>
2024-03-01 20:08:12 +0800

    实现搜索全球城市数据功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,21 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding =true
+    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.1")
+    val lifecycle_version = "2.7.0"
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
